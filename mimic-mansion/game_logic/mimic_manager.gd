1eea81d749e6c44e12a77fdcfbf5711081335f0f
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2026-01-22 11:03:45 +0100

    honstly kein plan was ich gemacht habe

--- mimic-mansion/game_logic/mimic_manager.gd
+++ mimic-mansion/game_logic/mimic_manager.gd
@@ -1,23 +1,22 @@
+class_name MimicManager
 extends Node3D
 
 
 @export_multiline() var mimic_pre_instructions: String
 @export_multiline() var mimic_post_instructions: String
-
-
-@onready var mimic_container: Node3D = %MimicContainer
-@onready var humgold: Evaluator = $Evaluator
+@export var humgold: Evaluator
 
 
 func _ready() -> void:
 	var mimic_statements = MimicStatements.new()
-	var num_questions: int = mimic_container.get_children().size()
+	var mimics: Array[Node] = get_tree().get_nodes_in_group("mimic")
+	var num_questions: int = mimics.size()
 	var statements: Array[Dictionary] = mimic_statements.parse_json(num_questions)
 	for i: int in range(num_questions):
 		#print(str(statements[i]))
-		mimic_container.get_child(i).set_instructions(mimic_pre_instructions + str(statements[i]) + mimic_post_instructions)
+		mimics[i].set_instructions(mimic_pre_instructions + str(statements[i]) + mimic_post_instructions)
 	
-	#print(questions)
 	humgold.num_questions = num_questions
 	humgold.statements = statements
+	await get_tree().process_frame # fuck this
 	humgold.set_evaluator_instructions()
