c78ca35cb998c1e71c24095e0df5b584134cda0c
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2026-02-02 17:57:36 +0100

    jakop

--- mimic-mansion/scenes/note_book.gd
+++ mimic-mansion/scenes/note_book.gd
@@ -4,23 +4,28 @@ extends Node3D
 var book_viewport: SubViewport
 var text_edit: TextEdit
 var anim_player: AnimationPlayer
+var menu: CanvasLayer
 var player
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
+	menu = $CanvasLayer
 	anim_player = $AnimationPlayer
 	if !book_viewport:
 		book_viewport =$".".find_child("book_viewport")
-		
+	
+	menu.hide()
 	text_edit = book_viewport.find_child("TextEdit")
-	player = get_tree().get_first_node_in_group("player")
+	player = get_tree().get_first_node_in_group("Player")
 		#text_edit.focus_mode = Control.FOCUS_ALL
 	#text_edit.mouse_filter = Control.MOUSE_FILTER_STOP
 	
 	
 func _input(event):
+	if visible && event.is_action_released("pause"):
+		deactivate_book()
 	if book_viewport && visible:
 		book_viewport.push_input(event)
 	elif event.is_action_pressed("open_book"):
@@ -33,33 +38,35 @@ func _input(event):
 	
 func activate_book():
 	show()
+	menu.show()
+	await get_tree().process_frame
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	book_viewport.gui_disable_input = false
 	book_viewport.handle_input_locally = true
 	await get_tree().process_frame
 	text_edit.grab_focus()
 	anim_player.play("appear")
-		
-	if player:
-		print("disabling player input while writing in notebook")
-		player.set_process(false)
-		player.set_physics_process(false)
-	else:
-		print("player group not found, cant disable controls while writing")
-
+	
+	player.set_process(false)
+	player.set_physics_process(false)
+	player.set_process_input(false)
+	player.set_process_unhandled_input(false)
+	
 func deactivate_book():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	book_viewport.gui_disable_input = true
 	book_viewport.handle_input_locally = false
 	await get_tree().process_frame
+	menu.hide()
 	text_edit.release_focus()
 	#anim_player.play("appear")
 	#TODO: disappear animation
 	hide()
 		
-	if player:
-		player.set_process(true)
-		player.set_physics_process(true)
+	player.set_process(true)
+	player.set_physics_process(true)
+	player.set_process_input(true)
+	player.set_process_unhandled_input(true)
 
 
 func _on_close_button_pressed() -> void:
